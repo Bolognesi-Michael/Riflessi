6eb8b53886a85baaf5e51e35cbe05cd6ac8fc522
Michael <michael.bolognesi@barsanti.edu.it>
2019-03-30 08:34:16 +0100

    bug fixing

--- riflessi.ino
+++ riflessi.ino
@@ -7,102 +7,100 @@ int ROSSO = 9;
 int TASTO = 13;
 int tempoled;
 int tempobuzzer;
+int tempomedio = tempoled + tempobuzzer;
 void setup() {
-  // put your setup code here, to run once:
 pinMode(LED, OUTPUT);
 pinMode(BUZZER, OUTPUT);
 pinMode(TASTO, INPUT);
 pinMode(ROSSO,OUTPUT);
 pinMode(VERDE,OUTPUT);
-LCD.begin(//);
-randomSeed(analogRead(0));
+LCD.begin(16,2);
 }
 
 void test(void){
-LCD.print("TEST DEI RIFLESSI");
+LCD.print("TEST RIFLESSI");
 delay(2000);
-LCD.clear;
-LCD.print("premere il tasto")
+LCD.clear();
+LCD.print("premere il tasto");
 LCD.setCursor(0,1);
 LCD.print("per iniziare");
-while(digitalRead TASTO == LOW){}
-LCD.clear;
+while(digitalRead (TASTO) == LOW){}
+LCD.clear();
 LCD.setCursor(0,0);
 LCD.print("TEST CON LED");
 delay(1000);
 }
 
 void led(void){
-  LCD.clear;
+  LCD.clear();
   LCD.print("3");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   LCD.print("2");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   LCD.print("1");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   int start = millis();
   digitalWrite(LED,HIGH);
-  while(digitalRead TASTO == LOW){}
+  while(digitalRead (TASTO) == LOW){}
   digitalWrite(LED,LOW);
   int t = millis();
   tempoled = t-start;
   LCD.print(tempoled);
   delay(2000);
-  LCD.clear;
+  LCD.clear();
 }
 void buzzer(void){
   LCD.print("TEST CON BUZZER");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   LCD.print("3");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   LCD.print("2");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   LCD.print("1");
   delay(1000);
-  LCD.clear;
+  LCD.clear();
   int startB = millis();
   tone(BUZZER,1000);
-  while(digitalRead TASTO == LOW){}
+  while(digitalRead (TASTO) == LOW){}
   noTone(BUZZER);
   int tB = millis();
-  tempobuzzer = t-start;
+  tempobuzzer = tB-startB;
   LCD.print(tempobuzzer);
   delay(2000);
-  LCD.clear;
+  LCD.clear();
 }
 void riflesso(void){
-  if((tempoled+tempobuzzer)/2 < 200 && tempoled+tempobuzzer)/2 >150)
+  if(tempomedio/2 < 200 && tempomedio/2 >150)
   {
     LCD.write("SEI PASSATO");
     LCD.setCursor(0,1);
-    LCD.write((tempoled+tempobuzzer)/2);
+    LCD.write(tempomedio/2);
     delay(2000);
-    LCD.clear;
+    LCD.clear();
   }
-    if((tempoled+tempobuzzer)/2 > 200)
+    if(tempomedio/2 > 200)
   {
     LCD.write("NON SEI PASSATO");
     LCD.setCursor(0,1);
-    LCD.write((tempoled+tempobuzzer)/2);
+    LCD.write(tempomedio/2);
     delay(2000);
-    LCD.clear;
+    LCD.clear();
   }
-    if((tempoled+tempobuzzer)/2 < 150)
+    if(tempomedio/2 < 150)
   {
     LCD.write("NOPE, BARATO");
     delay(2000);
-    LCD.clear;
+    LCD.clear();
   }
   
 }
 void loop() {
-  // put your main code here, to run repeatedly:
 test();
 led();
 buzzer();
